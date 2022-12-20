4b1fb0e00252eb4ca6c8b61b6ae52cf2793a0013
Mohamed H Noor <mohmed.hassannoor114@gmail.com>
2022-12-20 15:44:44 +0200

    Create lists dynamically in JS file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 import { style } from './style.css';
+
+const scoreLists = document.getElementById('score-lists');
+
+const lists = [
+  {
+    name: 'Mohamed',
+    score: 100
+  },
+  {
+    name: 'Ahmed',
+    score: 10
+  },
+  {
+    name: 'Ajaba',
+    score: 200
+  },
+  {
+    name: 'Abdi',
+    score: 90
+  },
+  {
+    name: 'Nima',
+    score: 1000
+  }
+];
+
+const displayList = () => {
+  lists.forEach((item) => {
+    const div = document.createElement('div');
+    div.classList.add('score-list');
+    div.innerHTML = `<p>${item.name}: ${item.score}</p>`;
+    scoreLists.appendChild(div);
+  });
+};
+
+displayList();
